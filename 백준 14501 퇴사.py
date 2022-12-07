41408cb8315e514cb3a66b9c390bdb6202a153f1
KKamjangE <ajm980718@gmail.com>
2022-12-08 00:42:04 +0900

    퇴사

--- 백준 14501 퇴사.py
+++ 백준 14501 퇴사.py
@@ -1,29 +1,20 @@
 import sys
 input = sys.stdin.readline
-import heapq
 
-N = int(input())
+N = int(input()) # 퇴사일
 
-dp = [0] * (N+1)
-day_price =[]
+dp = [0] * (N+1) # dp 테이블 생성
+work = [list(map(int, input().split())) for _ in range(N)] # work에 time, price 배열로 저장
 
-for i in range(N):
-    day, price = map(int, input().split())
-    heapq.heappush(day_price, (day, price, i+1))
-
-# print(day_price)
-
-for _ in range(N):
+for day in range(N-1, -1, -1): # 날짜 역순으로
+    time = work[day][0] # 현재 work time가져오기
     
-    end_time, price, day = heapq.heappop(day_price)
-    current_price = 0
-    
-    if day+end_time > N:
-        continue
-    
-    current_price = sum(dp[day:end_time])
+    if day + time <= N: # 현재 날짜 + time이 총 퇴사일보다 크지 않을 경우
+        price = work[day][1] # 현재 work price 가져오기
+        # 전날까지의 금액과 (현재 날짜 + work time) + price 비교해서 대입
+        dp[day] = max(dp[day + 1], dp[day + time] + price)
         
-    if dp[day+end_time] < price and current_price < price:
-        dp[day+end_time-1] += price
+    else: # 퇴사일을 넘어간다면
+        dp[day] = dp[day + 1] # 전날까지의 금액으로 대입
         
-print(sum(dp))
+print(dp[0]) # 마지막까지 합산된 금액 출력
