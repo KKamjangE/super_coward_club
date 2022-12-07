605074de35f9a81ccb27ec14da949ef6c76cb2bb
KKamjangE <ajm980718@gmail.com>
2022-12-07 22:44:32 +0900

    퇴사

---
+++ 백준 14501 퇴사.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+N = int(input())
+
+dp = [0] * (N+1)
+day_price =[]
+
+for i in range(N):
+    day, price = map(int, input().split())
+    heapq.heappush(day_price, (day-1, price, i+1))
+
+# print(day_price)
+
+for _ in range(N):
+    
+    end_time, price, day = heapq.heappop(day_price)
+    current_price = 0
+    
+    if day+end_time > N:
+        continue
+    
+    current_price = sum(dp[day:end_time+1])
+        
+    if dp[day+end_time] < price and current_price < price:
+        dp[day+end_time] += price
+        
+print(sum(dp))
