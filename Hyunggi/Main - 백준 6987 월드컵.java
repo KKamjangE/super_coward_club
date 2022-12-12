077a02107f16295c48ae073f9ae3b1d52339e589
HyunggiPark <phg9587@naver.com>
2022-12-13 02:25:45 +0900

    백준 월드컵 [6987] - 정답 코드 참조 - DFS 로 완전탐색 + 백트래킹 적용

---
+++ Hyunggi/Main - 백준 6987 월드컵.java
@@ -0,0 +1,82 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    static public void main(String[] args) throws IOException {
+        Solution ts = new Solution();
+
+        String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"};
+        String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};
+        String[] seller = {"young", "john", "tod", "emily", "mary"};
+        int[] amount = {12, 4, 2, 5, 10};
+
+        int[][] tables = new int[4][18];
+        for (int i = 0; i < tables.length; i++) {
+            String[] strings = br.readLine().split(" ");
+
+            for (int j = 0; j < strings.length; j++) {
+                tables[i][j] = Integer.parseInt(strings[j]);
+            }
+        }
+
+        ts.solution(tables);
+    }
+}
+
+//public class Main{
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int cordiNum = Integer.parseInt(br.readLine());
+//
+//        StringTokenizer st;
+//
+//        List<Coordinate> coordinateList = new ArrayList<Coordinate>();
+//
+//        for(int i = 0; i<cordiNum; i++){
+//            st = new StringTokenizer(br.readLine());
+//            int x = Integer.parseInt(st.nextToken());
+//            int y = Integer.parseInt(st.nextToken());
+//            coordinateList.add(new Coordinate(x, y));
+//        }
+//    }
+//}
+
+//안쓰는 코드 모음
+
+//        *Sacnner*
+//        Scanner scanner = new Scanner(System.in);
+//        String testCaseNumStr = scanner.nextLine();
+//        int testCaseNum = Integer.parseInt(testCaseNumStr);
+//        String[] testCaseArray = new String[testCaseNum];
+//
+//        for (int i = 0; i < testCaseNum; i++) {
+//            testCaseArray[i] = scanner.nextLine();
+//        }
+
+//        *StringTokenizer*
+//        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//        int stNum = Integer.parseInt(st.nextToken());
+//        int testValue = Integer.parseInt(br.readLine());
+
+//        *BufferedReader*
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int testCaseNum = Integer.parseInt(br.readLine());
+//        String[] testCase = new String[testCaseNum];
+//        for (int i = 0; i < testCaseNum; i++) {
+//            testCase[i] = br.readLine();
+//        }
+
+//    // N의 진짜 약수의 개수
+//    int testCase = Integer.parseInt(br.readLine());
+//    int[] inputArray = new int[testCase];
+//
+//    // N의 진짜 약수
+//    StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+//                for (int i = 0; i < testCase; i++) {
+//        inputArray[i] = Integer.parseInt(st.nextToken());
+//        }
+
